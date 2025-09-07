02f402a0e674a8793c6e0f9c0a2592265fb66424
Narain <Narain.gb@gmail.com>
2025-09-07 22:25:57 +0530

    Update files

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -281,7 +281,11 @@ const Home = () => {
             {filtered.length > 0 ? (
               <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                 {filtered.map((project) => (
-                  <ProjectCard key={project.id} project={project} />
+                  <ProjectCard 
+                    key={project.id} 
+                    project={project} 
+                    onDelete={() => {}} 
+                  />
                 ))}
               </div>
             ) : (
